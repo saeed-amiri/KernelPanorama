5e2616beaf5725dbe461a9ff66bedde0b090c0b7
saeed-amiri <samiri@uni-bremen.de>
2025-01-13 16:09:27 +0100

    update to print module info

--- src/hello.c
+++ src/hello.c
@@ -2,15 +2,24 @@
 # include <linux/module.h>
 # include <linux/kernel.h>
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Holden");
-MODULE_DESCRIPTION("A simple hello world kernel module");
-MODULE_VERSION("0.1");
+# define DRIVER_AUTHOR "Holden"
+# define DRIVER_DESC "A sample driver"
+# define DRIVER_LICENSE "GPL"
+# define DRIVER_VERSION "0.1"
+
+MODULE_LICENSE(DRIVER_LICENSE);
+MODULE_AUTHOR(DRIVER_AUTHOR);
+MODULE_DESCRIPTION(DRIVER_DESC);
+MODULE_VERSION(DRIVER_VERSION);
 
 // Caled when the module is loaded
 
 static int __init hello_init(void) {
     printk(KERN_INFO "Hello World: module loaded. \n");
+    pr_info("\tAuthor: %s\n", DRIVER_AUTHOR);
+    pr_info("\tDescription: %s\n", DRIVER_DESC);
+    pr_info("\tVersion: %s\n", DRIVER_VERSION);
+    pr_info("\tLicense: %s\n", DRIVER_LICENSE);
     return 0;
 }
 
